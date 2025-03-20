44f607a64911789bd03cf330bdde409858e019ce
Michael Joseph <mjoseph2013@gmail.com>
2025-03-20 15:13:18 -0400

    episodes interface, mapper, repo, & tests

---
+++ server/src/main/java/org/capstone/data/mappers/EpisodeMapper.java
@@ -0,0 +1,23 @@
+package org.capstone.data.mappers;
+
+import org.capstone.models.Episode;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EpisodeMapper implements RowMapper<Episode> {
+    @Override
+    public Episode mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Episode episode = new Episode();
+        episode.setId(rs.getInt("id"));
+        episode.setSeason(rs.getInt("season"));
+        episode.setEpisodeNumber(rs.getInt("episode_number"));
+        episode.setTitle(rs.getString("title"));
+        episode.setDescription(rs.getString("description"));
+        episode.setAirDate(rs.getDate("air_date").toLocalDate());
+        episode.setRating(rs.getBigDecimal("rating"));
+        episode.setShowId(rs.getInt("show_id"));
+        return episode;
+    }
+}
